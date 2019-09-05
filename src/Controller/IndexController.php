a3f7d5e14d9085027c3131a00e304bee8db45dae
KellyGauthier <laviolettek@gmail.com>
2019-09-05 16:00:33 +0200

    formulaire contact

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
 
 class IndexController extends AbstractController
 {
@@ -13,8 +15,22 @@ class IndexController extends AbstractController
     public function accueil()
     {
         return $this->render(
-            'index/accueil.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+            'index/accueil.html.twig', 
+            ['controller_name' => 'IndexController']
+    );
+    }
+/**
+ * @Route("/contact", name="contact")
+ */
+    public function contact()
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        return $this->render(
+        'index/contact.html.twig',
+        ['contactForm'=> $form->createView()]
+        );
     }
 }
+
