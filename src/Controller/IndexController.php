9fd22fc0f6a90c112297f370fb0c3b12ee04f7f8
KellyGauthier <laviolettek@gmail.com>
2019-09-05 16:33:40 +0200

    formulaire

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends AbstractController
 {
@@ -22,10 +23,19 @@ class IndexController extends AbstractController
 /**
  * @Route("/contact", name="contact")
  */
-    public function contact()
+    public function contact(Request $request)
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($contact);
+             $entityManager->flush();
+
+             //TODO Rediriger vers une page de confirmation
+        }
 
         return $this->render(
         'index/contact.html.twig',
