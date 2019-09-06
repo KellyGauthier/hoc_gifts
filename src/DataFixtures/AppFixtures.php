0e087278f4b7dfe13428e98a15ab013740e2aa48
KellyGauthier <laviolettek@gmail.com>
2019-09-06 16:56:35 +0200

    secure authentification

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,33 @@
 namespace App\DataFixtures;
 
 use App\Entity\Gift;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+
+        $user->setEmail('bob@bob.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->encoder->encodePassword(
+                $user,
+                "blablabla"
+            ));
+
+        $manager->persist($user);
+        $manager->flush();
+
         $faker = Factory::create('fr_FR');
 
         for($i=1; $i < 15; $i++){
