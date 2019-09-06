f660735686fa5655a4d7d2d30f483af241a770b0
KellyGauthier <laviolettek@gmail.com>
2019-09-06 12:16:51 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Gift;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
 
+        for($i=1; $i < 15; $i++){
+        // Je crée un objet
+        $gift = new Gift();
+        //J'affecte ses atributs
+        $gift->setTitle($faker->sentence(5, true))
+            ->setDescription($faker->paragraph(5, true));
+
+        // J'indique à mon gestionnaire d'entités que je veux insérer cet objet en BDD
+        $manager->persist($gift);
+    }
+        //Je valide les modifications effectuées (insertions, modifications sur des enregistrements,...) dans la BDD
         $manager->flush();
     }
 }
